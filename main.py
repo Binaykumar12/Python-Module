34343d612a432ad02d8f2c7134d9fd126043aabd
Binaykumar12 <binaykumarkurmi78@gmail.com>
2024-07-25 12:48:56 +0545

    .

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import mymodule as mm
 
 # print(mm.greet("Binay"))
 mm.greet("Binay")
+
+print(mm.age(18))
+print(mm.add(4,5))
+
+
+print(mm.calculator(5,15, 'add'))
+print(mm.calculator(4, 15, 'sub'))
+print(mm.calculator(42, 5, 'mul'))
+print(mm.calculator(4,25, 'div'))
--- mymodule.py
+++ mymodule.py
@@ -1,3 +1,23 @@
 def greet(name):
     # return f"Hello,{name}!"
     print(f"Hello,{name}!")
+
+
+def age(age):
+    return int(age)
+
+def add (a,b):
+    result = a + b
+    return f"The addition of both numbers is {result}"
+
+def calculator(a, b, operation):
+    if operation == 'add':
+        return a + b
+    elif operation == 'sub':
+        return a - b
+    elif operation == 'mul':
+        return a * b
+    elif operation == 'div':
+        return a / b
+    else:
+        return "Invalid operation. Choose 'add', 'sub', 'mul', or 'div'."
